2db58c67dcb4fe2fac9691dfd0bc3055e23344de
tom.debek@gmail.com <tom.debek@gmail.com>
2019-10-21 21:10:27 +0200

    Add new structure views and main note

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,55 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @notes = Note.all
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  def update
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    if @note.destroy
+    redirect_to notes_path
+    else
+      flash[:notice] = "Something is wrong. Please try again"
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+end
